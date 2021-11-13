f5ea2ffcfcf4b8c296ef28302d1ca01283b1269c
ta8i2chi8 <moribook0828@gmail.com>
2021-11-13 11:50:06 +0900

    [fix] gifに現在のepoch数を追加

--- visualize_gif.py
+++ visualize_gif.py
@@ -1,17 +1,28 @@
-from PIL import Image
+import sys
+
+from PIL import Image, ImageDraw, ImageFont
 import glob
 from visualize import plot
 from genotypes import Genotype
 
 
-def resize_img(img, width, height):
+def resize_img(img, epoch, width, height):
     # 作りたいサイズの白紙画像を生成
-    new_img = Image.new(img.mode, (width, height), (255, 255, 255))
+    new_img = Image.new(img.mode, (width, height), "white")
 
     # 白紙画像に元画像をペースト
-    left_loc = (width / 2) - (img.size[0] / 2)
-    top_loc = (height / 2) - (img.size[1] / 2)
-    new_img.paste(img, (int(left_loc), int(top_loc)))
+    left_loc_img = (width / 2) - (img.size[0] / 2)
+    top_loc_img = (height / 2) - (img.size[1] / 2)
+    new_img.paste(img, (int(left_loc_img), int(top_loc_img)))
+
+    # epoch数のテキストを画像に描画
+    draw = ImageDraw.Draw(new_img)
+    font = ImageFont.truetype("arial.ttf", 40)
+    word = f"epoch {epoch}"
+    w, _ = draw.textsize(word)
+    left_loc_txt = (width - w) / 2
+    top_loc_txt = (height / 2) + (img.size[1] / 2) + 10
+    draw.text((int(left_loc_txt), int(top_loc_txt)), word, fill="black", font=font)
 
     return new_img
 
@@ -22,7 +33,7 @@ def get_genotypes(log_path):
         raw_lines = [line.strip() for line in f.readlines()]
 
         genotypes = []
-        for i in range(7):
+        for i in range(50):
             genotypes.append(eval(raw_lines[4 + i * 12][35:]))
 
     return genotypes
@@ -31,12 +42,16 @@ def get_genotypes(log_path):
 def create_gif(img_path, output_name):
     files = sorted(glob.glob(img_path + '/*.png'))
     images = list(map(lambda file: Image.open(file), files))
-    resized_images = [resize_img(image, 1500, 600) for image in images]
+    resized_images = [resize_img(image, n + 1, 1500, 600) for n, image in enumerate(images)]
     resized_images[0].save(output_name, save_all=True, append_images=resized_images[1:], duration=300, loop=0)
 
 
 if __name__ == '__main__':
-    genos = get_genotypes('./logs/search-EXP-20211112-113450')
+    if len(sys.argv) != 2:
+        print("usage:\n python {} FOLDER_NAME_DATE".format(sys.argv[0]))
+        sys.exit(1)
+
+    genos = get_genotypes(f'./logs/search-EXP-{sys.argv[1]}')
 
     # Genotypeからpngを生成（各エポックに対して）
     for i, geno in enumerate(genos):
